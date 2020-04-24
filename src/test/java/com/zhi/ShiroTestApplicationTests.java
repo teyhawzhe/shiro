32c30c6de876460695e0806642bed1303c4430de
lovius215 <loviusc@gmail.com>
2020-04-24 14:13:59 +0800

    initial

---
+++ src/test/java/com/zhi/ShiroTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
